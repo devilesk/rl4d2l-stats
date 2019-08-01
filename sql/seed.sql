645316b40a386ee854175d11b0d075ebeda22dd9
devilesk <devilesk@gmail.com>
2019-08-01 11:42:35 +0000

    rename insert_match to insert_fake_match

--- sql/seed.sql
+++ sql/seed.sql
@@ -17,9 +17,9 @@ LINES TERMINATED BY '\n';
 
 DELIMITER //
 
-DROP PROCEDURE IF EXISTS insert_match;
+DROP PROCEDURE IF EXISTS insert_fake_match;
 
-CREATE PROCEDURE  insert_match(IN matchId INT, IN maps VARCHAR(256))
+CREATE PROCEDURE  insert_fake_match(IN matchId INT, IN maps VARCHAR(256))
 BEGIN
 
 SET @matchId = matchId;
@@ -554,202 +554,202 @@ END WHILE;
 END //
 DELIMITER ;
 
-CALL insert_match(
+CALL insert_fake_match(
 1562680772,
 'c1m1_hotel,c1m2_streets,c1m3_mall,c1m4_atrium,'
 );
 
-CALL insert_match(
+CALL insert_fake_match(
 1562680773,
 'c2m1_highway,c2m2_fairgrounds,c2m3_coaster,c2m4_barns,c2m5_concert,'
 );
 
-CALL insert_match(
+CALL insert_fake_match(
 1562680774,
 'c3m1_plankcountry,c3m2_swamp,c3m3_shantytown,c3m4_plantation,'
 );
 
-CALL insert_match(
+CALL insert_fake_match(
 1562680775,
 'c4m1_milltown_a,c4m2_sugarmill_a,c4m3_sugarmill_b,c4m4_milltown_b,c4m5_milltown_escape,'
 );
 
-CALL insert_match(
+CALL insert_fake_match(
 1562680776,
 'c5m1_waterfront,c5m2_park,c5m3_cemetery,c5m4_quarter,c5m5_bridge,'
 );
 
-CALL insert_match(
+CALL insert_fake_match(
 1562680777,
 'c1m1_hotel,c1m2_streets,c1m3_mall,c1m4_atrium,'
 );
 
-CALL insert_match(
+CALL insert_fake_match(
 1562680778,
 'c2m1_highway,c2m2_fairgrounds,c2m3_coaster,c2m4_barns,c2m5_concert,'
 );
 
-CALL insert_match(
+CALL insert_fake_match(
 1562680779,
 'c3m1_plankcountry,c3m2_swamp,c3m3_shantytown,c3m4_plantation,'
 );
 
-CALL insert_match(
+CALL insert_fake_match(
 1562680780,
 'c4m1_milltown_a,c4m2_sugarmill_a,c4m3_sugarmill_b,c4m4_milltown_b,c4m5_milltown_escape,'
 );
 
-CALL insert_match(
+CALL insert_fake_match(
 1562680781,
 'c5m1_waterfront,c5m2_park,c5m3_cemetery,c5m4_quarter,c5m5_bridge,'
 );
 
-CALL insert_match(
+CALL insert_fake_match(
 1562680782,
 'c1m1_hotel,c1m2_streets,c1m3_mall,c1m4_atrium,'
 );
 
-CALL insert_match(
+CALL insert_fake_match(
 1562680783,
 'c2m1_highway,c2m2_fairgrounds,c2m3_coaster,c2m4_barns,c2m5_concert,'
 );
 
-CALL insert_match(
+CALL insert_fake_match(
 1562680784,
 'c3m1_plankcountry,c3m2_swamp,c3m3_shantytown,c3m4_plantation,'
 );
 
-CALL insert_match(
+CALL insert_fake_match(
 1562680785,
 'c4m1_milltown_a,c4m2_sugarmill_a,c4m3_sugarmill_b,c4m4_milltown_b,c4m5_milltown_escape,'
 );
 
-CALL insert_match(
+CALL insert_fake_match(
 1562680786,
 'c5m1_waterfront,c5m2_park,c5m3_cemetery,c5m4_quarter,c5m5_bridge,'
 );
 
-CALL insert_match(
+CALL insert_fake_match(
 1562680787,
 'c1m1_hotel,c1m2_streets,c1m3_mall,c1m4_atrium,'
 );
 
-CALL insert_match(
+CALL insert_fake_match(
 1562680788,
 'c2m1_highway,c2m2_fairgrounds,c2m3_coaster,c2m4_barns,c2m5_concert,'
 );
 
-CALL insert_match(
+CALL insert_fake_match(
 1562680789,
 'c3m1_plankcountry,c3m2_swamp,c3m3_shantytown,c3m4_plantation,'
 );
 
-CALL insert_match(
+CALL insert_fake_match(
 1562680790,
 'c4m1_milltown_a,c4m2_sugarmill_a,c4m3_sugarmill_b,c4m4_milltown_b,c4m5_milltown_escape,'
 );
 
-CALL insert_match(
+CALL insert_fake_match(
 1562680791,
 'c5m1_waterfront,c5m2_park,c5m3_cemetery,c5m4_quarter,c5m5_bridge,'
 );
 
-CALL insert_match(
+CALL insert_fake_match(
 1562680792,
 'c1m1_hotel,c1m2_streets,c1m3_mall,c1m4_atrium,'
 );
 
-CALL insert_match(
+CALL insert_fake_match(
 1562680793,
 'c2m1_highway,c2m2_fairgrounds,c2m3_coaster,c2m4_barns,c2m5_concert,'
 );
 
-CALL insert_match(
+CALL insert_fake_match(
 1562680794,
 'c3m1_plankcountry,c3m2_swamp,c3m3_shantytown,c3m4_plantation,'
 );
 
-CALL insert_match(
+CALL insert_fake_match(
 1562680795,
 'c4m1_milltown_a,c4m2_sugarmill_a,c4m3_sugarmill_b,c4m4_milltown_b,c4m5_milltown_escape,'
 );
 
-CALL insert_match(
+CALL insert_fake_match(
 1562680796,
 'c5m1_waterfront,c5m2_park,c5m3_cemetery,c5m4_quarter,c5m5_bridge,'
 );
 
-CALL insert_match(
+CALL insert_fake_match(
 1562680797,
 'c1m1_hotel,c1m2_streets,c1m3_mall,c1m4_atrium,'
 );
 
-CALL insert_match(
+CALL insert_fake_match(
 1562680798,
 'c2m1_highway,c2m2_fairgrounds,c2m3_coaster,c2m4_barns,c2m5_concert,'
 );
 
-CALL insert_match(
+CALL insert_fake_match(
 1562680799,
 'c3m1_plankcountry,c3m2_swamp,c3m3_shantytown,c3m4_plantation,'
 );
 
-CALL insert_match(
+CALL insert_fake_match(
 1562680800,
 'c4m1_milltown_a,c4m2_sugarmill_a,c4m3_sugarmill_b,c4m4_milltown_b,c4m5_milltown_escape,'
 );
 
-CALL insert_match(
+CALL insert_fake_match(
 1562680801,
 'c5m1_waterfront,c5m2_park,c5m3_cemetery,c5m4_quarter,c5m5_bridge,'
 );
 
-CALL insert_match(
+CALL insert_fake_match(
 1562680802,
 'c1m1_hotel,c1m2_streets,c1m3_mall,c1m4_atrium,'
 );
 
-CALL insert_match(
+CALL insert_fake_match(
 1562680803,
 'c2m1_highway,c2m2_fairgrounds,c2m3_coaster,c2m4_barns,c2m5_concert,'
 );
 
-CALL insert_match(
+CALL insert_fake_match(
 1562680804,
 'c3m1_plankcountry,c3m2_swamp,c3m3_shantytown,c3m4_plantation,'
 );
 
-CALL insert_match(
+CALL insert_fake_match(
 1562680805,
 'c4m1_milltown_a,c4m2_sugarmill_a,c4m3_sugarmill_b,c4m4_milltown_b,c4m5_milltown_escape,'
 );
 
-CALL insert_match(
+CALL insert_fake_match(
 1562680806,
 'c5m1_waterfront,c5m2_park,c5m3_cemetery,c5m4_quarter,c5m5_bridge,'
 );
 
-CALL insert_match(
+CALL insert_fake_match(
 1562680807,
 'c1m1_hotel,c1m2_streets,c1m3_mall,c1m4_atrium,'
 );
 
-CALL insert_match(
+CALL insert_fake_match(
 1562680808,
 'c2m1_highway,c2m2_fairgrounds,c2m3_coaster,c2m4_barns,c2m5_concert,'
 );
 
-CALL insert_match(
+CALL insert_fake_match(
 1562680809,
 'c3m1_plankcountry,c3m2_swamp,c3m3_shantytown,c3m4_plantation,'
 );
 
-CALL insert_match(
+CALL insert_fake_match(
 1562680810,
 'c4m1_milltown_a,c4m2_sugarmill_a,c4m3_sugarmill_b,c4m4_milltown_b,c4m5_milltown_escape,'
 );
 
-CALL insert_match(
+CALL insert_fake_match(
 1562680811,
 'c5m1_waterfront,c5m2_park,c5m3_cemetery,c5m4_quarter,c5m5_bridge,'
 );
