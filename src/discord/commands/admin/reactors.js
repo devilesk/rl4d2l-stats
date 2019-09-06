1c8b80ed6f369f9cb29bf14fbac9caac3e7361e1
devilesk <devilesk@gmail.com>
2019-09-06 22:15:24 +0000

    add reactors command

---
+++ src/discord/commands/admin/reactors.js
@@ -0,0 +1,66 @@
+const { Command } = require('discord.js-commando');
+const { Collection } = require('discord.js');
+const Promise = require('bluebird');
+const msgFromAdmin = require('../../msgFromAdmin');
+const config = require('../../config');
+const logger = require('../../../cli/logger');
+
+const partition = (arr, length) => {
+    const result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (i % length === 0) {
+            result.push([]);
+        }
+        result[result.length - 1].push(arr[i]);
+    }
+    return result;
+};
+
+class ReactorsCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'reactors',
+            group: 'admin',
+            memberName: 'reactors',
+            description: 'List users that have reacted to a message.',
+            args: [
+                {
+                    key: 'channel',
+                    prompt: 'Channel mention',
+                    type: 'channel',
+                },
+                {
+                    key: 'messageId',
+                    prompt: 'Message ID',
+                    type: 'string',
+                },
+            ],
+        });
+    }
+
+    hasPermission(msg) {
+        return msgFromAdmin(msg);
+    }
+
+    async run(msg, { channel, messageId }) {
+        let fetchedMsg;
+        try {
+            fetchedMsg = await channel.fetchMessage(messageId);
+        }
+        catch (e) {
+            logger.error(e);
+            return msg.say(`Message ${messageId} in ${channel} not found.`);
+        }
+        const users = await Promise.reduce(fetchedMsg.reactions.array(), async (users, reaction) => {
+            const fetchedUsers = await reaction.fetchUsers();
+            logger.debug(`fetched message ${fetchedMsg.id} reaction ${reaction.emoji} users ${fetchedUsers.size}`);
+            return users.concat(fetchedUsers);
+        }, new Collection());
+        const groups = partition(users.array().map(user => msg.guild.member(user).displayName).sort((a, b) => a.localeCompare(b)), 20);
+        for (const group of groups) {
+            await msg.say((group === groups[0] ? `https://discordapp.com/channels/${msg.guild.id}/${channel.id}/${fetchedMsg.id} ${users.size} unique reactors:\n` : '') + `${group.join('\n')}`);
+        }
+    }
+}
+
+module.exports = ReactorsCommand;
