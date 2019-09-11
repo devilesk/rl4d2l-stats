f8de3b0b14c74e8c60ca8f0ef30f296fba85ce8b
devilesk <devilesk@gmail.com>
2019-09-11 20:03:38 +0000

    add top pingers command

---
+++ src/discord/commands/league/toppingers.js
@@ -0,0 +1,63 @@
+const { Command } = require('discord.js-commando');
+const { RichEmbed } = require('discord.js');
+const config = require('../../config');
+const { msgHasL4DMention } = require('../../util');
+
+class TopPingers extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'toppingers',
+            group: 'league',
+            memberName: 'toppingers',
+            description: 'Top inhouse role pingers.',
+        });
+    }
+
+    hasPermission(msg) {
+        return this.client.isOwner(msg.author);
+    }
+
+    async run(msg) {
+        let results = {};
+        let messages;
+        let lastMessage;
+        const channel = msg.guild.channels.find(channel => channel.name === config.settings.inhouseChannel);
+        const reply = await msg.say('Processing...');
+        do {
+            messages = await channel.fetchMessages({
+                limit: 100,
+                before: lastMessage ? lastMessage.id : null,
+            });
+            lastMessage = messages.array()[messages.size - 1];
+            for (const _msg of messages.array()) {
+                const message = await channel.fetchMessage(_msg.id);
+                if (msgHasL4DMention(message)) {
+                    results[message.author.id] = results[message.author.id] || {
+                        total: 0,
+                        success: 0,
+                        displayName: msg.guild.members.get(message.author.id).displayName,
+                    };
+                    results[message.author.id].total++;
+                    if (message.reactions.array().some(reaction => reaction.name === '✅')) {
+                        results[message.author.id].success++;
+                    }
+                }
+            }
+        } while (messages.size);
+        results = Object.values(results).sort((a, b) => (b.success / b.total) - (a.success / a.total));
+        if (results.length) {
+            const embed = new RichEmbed()
+                .setTitle(`**Top ${config.settings.inhouseRole} Pingers**`)
+                .setColor(0x972323);
+            const title = `Name | Total | Success | %`;
+            const content = results.map(result => `${result.displayName} | ${result.total} | ${result.success} | ${+(result.success / result.total).toFixed(2)}`).join('\n');
+            embed.addField(title, content, false);
+            reply.edit(embed);
+        }
+        else {
+            reply.edit(`No ${config.settings.inhouseRole} pings found.`);
+        }
+    }
+}
+
+module.exports = TopPingers;
