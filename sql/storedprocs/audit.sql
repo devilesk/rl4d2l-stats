b4eed00089bd1108033ea2885b449fd2ec1a02ff
devilesk <devilesk@gmail.com>
2019-09-16 13:30:09 -0400

    update audit script to exclude deleted rounds, add query name to results, add map count query

--- sql/storedprocs/audit.sql
+++ sql/storedprocs/audit.sql
@@ -6,8 +6,9 @@ CREATE PROCEDURE audit()
 BEGIN
 
     -- Matches with correct campaign count and correct number of rounds, but not starting from round 1
-    SELECT a.matchId, MIN(a.round), MAX(a.round), MAX(a.round) - MIN(a.round), COUNT(a.round), COUNT(DISTINCT b.campaign), MAX(b.campaign), MAX(b.round)
+    SELECT a.matchId as 'bad round start', MIN(a.round), MAX(a.round), MAX(a.round) - MIN(a.round), COUNT(a.round), COUNT(DISTINCT b.campaign), MAX(b.campaign), MAX(b.round)
     FROM round a JOIN maps b ON a.map = b.map
+    WHERE a.deleted = 0
     GROUP BY a.matchId
     HAVING COUNT(DISTINCT b.campaign) = 1
     AND MAX(a.round) - MIN(a.round) + 1 = MAX(b.round)
@@ -15,20 +16,30 @@ BEGIN
     AND MIN(a.round) <> 1;
 
     -- Matches with incorrect campaign count. Need to run fixmatch.sql to split them.
-    SELECT a.matchId, COUNT(DISTINCT b.campaign), GROUP_CONCAT(DISTINCT b.campaign)
+    SELECT a.matchId as 'bad campaign count', COUNT(DISTINCT b.campaign), GROUP_CONCAT(DISTINCT b.campaign)
     FROM round a JOIN maps b ON a.map = b.map
+    WHERE a.deleted = 0
     GROUP BY a.matchId
     HAVING COUNT(DISTINCT b.campaign) <> 1;
 
-    -- Matches with incorrect round count. Incomplete match?
-    SELECT a.matchId, MAX(a.round) - MIN(a.round) + 1, MAX(b.round)
+    -- Matches with incorrect map count. Incomplete match?
+    SELECT a.matchId as 'bad map count', MAX(a.round), MIN(a.round), MAX(a.round) - MIN(a.round) + 1, MAX(b.round), COUNT(DISTINCT a.map), COUNT(DISTINCT b.map)
     FROM round a JOIN maps b ON a.map = b.map
+    WHERE a.deleted = 0
+    GROUP BY a.matchId
+    HAVING COUNT(DISTINCT a.map) <> COUNT(DISTINCT b.map);
+
+    -- Matches with incorrect round count. Incomplete match?
+    SELECT a.matchId as 'bad round start/end count', MAX(a.round), MIN(a.round), MAX(a.round) - MIN(a.round) + 1, MAX(b.round)
+    FROM round a JOIN maps b ON a.map = b.map
+    WHERE a.deleted = 0
     GROUP BY a.matchId
     HAVING MAX(a.round) - MIN(a.round) + 1 <> MAX(b.round);
 
     -- Matches with incorrect round count. Incomplete match?
-    SELECT a.matchId, COUNT(a.round), MAX(b.round)
+    SELECT a.matchId as 'bad round total count', COUNT(a.round), MAX(b.round), 2 * MAX(b.round)
     FROM round a JOIN maps b ON a.map = b.map
+    WHERE a.deleted = 0
     GROUP BY a.matchId
     HAVING COUNT(a.round) <> 2 * MAX(b.round);
 
