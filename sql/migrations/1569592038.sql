a6b07c69d73acb1ae9fc2359c40f310d4233c7a9
devilesk <devilesk@gmail.com>
2019-09-27 21:45:00 +0000

    add team logos

---
+++ sql/migrations/1569592038.sql
@@ -0,0 +1,11 @@
+UPDATE team
+SET logoImage = '/img/teamlogos/1/Autoattacks.png'
+WHERE season = 1 AND steamid_0 = 'STEAM_1:1:43423378';
+
+UPDATE team
+SET logoImage = '/img/teamlogos/1/Wicket.jpg'
+WHERE season = 1 AND steamid_0 = 'STEAM_1:0:41878654';
+
+UPDATE team
+SET logoImage = '/img/teamlogos/2/Gofu.png'
+WHERE season = 2 AND steamid_0 = 'STEAM_1:1:128593';
