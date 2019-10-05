25e98dca72547aaf1d0de2c0a078e35887702e34
devilesk <devilesk@gmail.com>
2019-10-05 18:18:53 +0000

    add advanced query filter to matches tab

---
+++ src/js/queryBuilder.js
@@ -0,0 +1,175 @@
+import QueryBuilder from 'jQuery-QueryBuilder';
+
+QueryBuilder.templates.group = '\
+<div id="{{= it.group_id }}" class="rules-group-container card bg-light"> \
+  <div class="rules-group-header"> \
+    {{? it.level>1 }} \
+      <button type="button" class="btn btn-sm btn-danger float-right ml-1" data-delete="group"> \
+        <i class="{{= it.icons.remove_group }}"></i> {{= it.translate("delete_group") }} \
+      </button> \
+    {{?}} \
+    <div class="btn-group ml-1 float-right group-actions"> \
+      <button type="button" class="btn btn-sm btn-primary" data-add="rule"> \
+        <i class="{{= it.icons.add_rule }}"></i> {{= it.translate("add_rule") }} \
+      </button> \
+      {{? it.settings.allow_groups===-1 || it.settings.allow_groups>=it.level }} \
+        <button type="button" class="btn btn-sm btn-primary" data-add="group"> \
+          <i class="{{= it.icons.add_group }}"></i> {{= it.translate("add_group") }} \
+        </button> \
+      {{?}} \
+    </div> \
+    <div class="btn-group group-conditions"> \
+      {{~ it.conditions: condition }} \
+        <label class="btn btn-sm btn-primary"> \
+          <input type="radio" name="{{= it.group_id }}_cond" value="{{= condition }}"> {{= it.translate("conditions", condition) }} \
+        </label> \
+      {{~}} \
+    </div> \
+    {{? it.settings.display_errors }} \
+      <div class="error-container"><i class="{{= it.icons.error }}"></i></div> \
+    {{?}} \
+  </div> \
+  <div class=rules-group-body> \
+    <div class=rules-list></div> \
+  </div> \
+</div>';
+
+QueryBuilder.templates.rule = '\
+<div id="{{= it.rule_id }}" class="rule-container d-flex flex-row card"> \
+  {{? it.settings.display_errors }} \
+    <div class="error-container"><i class="{{= it.icons.error }}"></i></div> \
+  {{?}} \
+  <div class="rule-filter-container"></div> \
+  <div class="rule-operator-container"></div> \
+  <div class="rule-value-container"></div> \
+  <div class="rule-header ml-auto"> \
+    <div class="btn-group pull-right rule-actions"> \
+      <button type="button" class="btn btn-sm btn-danger" data-delete="rule"> \
+        <i class="{{= it.icons.remove_rule }}"></i> {{= it.translate("delete_rule") }} \
+      </button> \
+    </div> \
+  </div> \
+</div>';
+
+QueryBuilder.templates.filterSelect = '\
+{{ var optgroup = null; }} \
+<select class="form-control form-control-sm" name="{{= it.rule.id }}_filter"> \
+  {{? it.settings.display_empty_filter }} \
+    <option value="-1">{{= it.settings.select_placeholder }}</option> \
+  {{?}} \
+  {{~ it.filters: filter }} \
+    {{? optgroup !== filter.optgroup }} \
+      {{? optgroup !== null }}</optgroup>{{?}} \
+      {{? (optgroup = filter.optgroup) !== null }} \
+        <optgroup label="{{= it.translate(it.settings.optgroups[optgroup]) }}"> \
+      {{?}} \
+    {{?}} \
+    <option value="{{= filter.id }}" {{? filter.icon}}data-icon="{{= filter.icon}}"{{?}}>{{= it.translate(filter.label) }}</option> \
+  {{~}} \
+  {{? optgroup !== null }}</optgroup>{{?}} \
+</select>';
+
+QueryBuilder.templates.operatorSelect = '\
+{{? it.operators.length === 1 }} \
+<span> \
+{{= it.translate("operators", it.operators[0].type) }} \
+</span> \
+{{?}} \
+{{ var optgroup = null; }} \
+<select class="form-control form-control-sm {{? it.operators.length === 1 }}hide{{?}}" name="{{= it.rule.id }}_operator"> \
+  {{~ it.operators: operator }} \
+    {{? optgroup !== operator.optgroup }} \
+      {{? optgroup !== null }}</optgroup>{{?}} \
+      {{? (optgroup = operator.optgroup) !== null }} \
+        <optgroup label="{{= it.translate(it.settings.optgroups[optgroup]) }}"> \
+      {{?}} \
+    {{?}} \
+    <option value="{{= operator.type }}" {{? operator.icon}}data-icon="{{= operator.icon}}"{{?}}>{{= it.translate("operators", operator.type) }}</option> \
+  {{~}} \
+  {{? optgroup !== null }}</optgroup>{{?}} \
+</select>';
+
+QueryBuilder.templates.ruleValueSelect = '\
+{{ var optgroup = null; }} \
+<select class="form-control form-control-sm" name="{{= it.name }}" {{? it.rule.filter.multiple }}multiple{{?}}> \
+  {{? it.rule.filter.placeholder }} \
+    <option value="{{= it.rule.filter.placeholder_value }}" disabled selected>{{= it.rule.filter.placeholder }}</option> \
+  {{?}} \
+  {{~ it.rule.filter.values: entry }} \
+    {{? optgroup !== entry.optgroup }} \
+      {{? optgroup !== null }}</optgroup>{{?}} \
+      {{? (optgroup = entry.optgroup) !== null }} \
+        <optgroup label="{{= it.translate(it.settings.optgroups[optgroup]) }}"> \
+      {{?}} \
+    {{?}} \
+    <option value="{{= entry.value }}">{{= entry.label }}</option> \
+  {{~}} \
+  {{? optgroup !== null }}</optgroup>{{?}} \
+</select>';
+
+QueryBuilder.prototype.getRuleInput = function(rule, value_id) {
+    var filter = rule.filter;
+    var validation = rule.filter.validation || {};
+    var name = rule.id + '_value_' + value_id;
+    var c = filter.vertical ? ' class=block' : '';
+    var h = '';
+
+    if (typeof filter.input == 'function') {
+        h = filter.input.call(this, rule, name);
+    }
+    else {
+        switch (filter.input) {
+            case 'radio':
+            case 'checkbox':
+                Utils.iterateOptions(filter.values, function(key, val) {
+                    h += '<label' + c + '><input type="' + filter.input + '" name="' + name + '" value="' + key + '"> ' + val + '</label> ';
+                });
+                break;
+
+            case 'select':
+                h = this.getRuleValueSelect(name, rule);
+                break;
+
+            case 'textarea':
+                h += '<textarea class="form-control form-control-sm" name="' + name + '"';
+                if (filter.size) h += ' cols="' + filter.size + '"';
+                if (filter.rows) h += ' rows="' + filter.rows + '"';
+                if (validation.min !== undefined) h += ' minlength="' + validation.min + '"';
+                if (validation.max !== undefined) h += ' maxlength="' + validation.max + '"';
+                if (filter.placeholder) h += ' placeholder="' + filter.placeholder + '"';
+                h += '></textarea>';
+                break;
+
+            case 'number':
+                h += '<input class="form-control form-control-sm" type="number" name="' + name + '"';
+                if (validation.step !== undefined) h += ' step="' + validation.step + '"';
+                if (validation.min !== undefined) h += ' min="' + validation.min + '"';
+                if (validation.max !== undefined) h += ' max="' + validation.max + '"';
+                if (filter.placeholder) h += ' placeholder="' + filter.placeholder + '"';
+                if (filter.size) h += ' size="' + filter.size + '"';
+                h += '>';
+                break;
+
+            default:
+                h += '<input class="form-control form-control-sm" type="text" name="' + name + '"';
+                if (filter.placeholder) h += ' placeholder="' + filter.placeholder + '"';
+                if (filter.type === 'string' && validation.min !== undefined) h += ' minlength="' + validation.min + '"';
+                if (filter.type === 'string' && validation.max !== undefined) h += ' maxlength="' + validation.max + '"';
+                if (filter.size) h += ' size="' + filter.size + '"';
+                h += '>';
+        }
+    }
+
+    /**
+     * Modifies the raw HTML of the rule's input
+     * @event changer:getRuleInput
+     * @memberof QueryBuilder
+     * @param {string} html
+     * @param {Rule} rule
+     * @param {string} name - the name that the input must have
+     * @returns {string}
+     */
+    return this.change('getRuleInput', h, rule, name);
+};
+
+export default QueryBuilder;
--- src/js/tabs/matches.js
+++ src/js/tabs/matches.js
@@ -3,6 +3,69 @@ import boldCellRenderer from '../util/boldCellRenderer';
 import matchIdRenderer from '../util/matchIdRenderer';
 import BaseTab from './base';
 import HandsontableConfig from '../handsontable.config';
+import QueryBuilder from '../queryBuilder';
+
+const colMap = {
+    teamA: 2,
+    teamB: 6,
+    result: 4,
+    teamATotal: 3,
+    teamBTotal: 5,
+    pointDiff: 7,
+    matchId: 0,
+    map: 1,
+}
+
+const labelMap = {
+    teamA: 'Team A',
+    teamB: 'Team B',
+    result: 'Result',
+    teamATotal: 'Points A',
+    teamBTotal: 'Points B',
+    pointDiff: 'Pt. Diff.',
+    matchId: 'Match ID',
+    map: 'Map',
+}
+
+const getRowValue = (row, id) => row[colMap[id]];
+
+const opFns = {
+    contains: (rowVal, ruleVal) => rowVal.indexOf(ruleVal) !== -1,
+    not_contains: (rowVal, ruleVal) => rowVal.indexOf(ruleVal) === -1,
+    equal: (rowVal, ruleVal) => rowVal === ruleVal,
+    not_equal: (rowVal, ruleVal) => rowVal !== ruleVal,
+    less: (rowVal, ruleVal) => rowVal < ruleVal,
+    less_or_equal: (rowVal, ruleVal) => rowVal <= ruleVal,
+    greater: (rowVal, ruleVal) => rowVal > ruleVal,
+    greater_or_equal: (rowVal, ruleVal) => rowVal >= ruleVal,
+    between: (rowVal, [ruleMinVal, ruleMaxVal]) => rowVal >= ruleMinVal && rowVal <= ruleMaxVal,
+    not_between: (rowVal, [ruleMinVal, ruleMaxVal]) => rowVal < ruleMinVal || rowVal > ruleMaxVal,
+};
+
+const opStringFns = {
+    contains: (rowVal, ruleVal) => `${rowVal} contains ${ruleVal}`,
+    not_contains: (rowVal, ruleVal) => `${rowVal} not contains ${ruleVal}`,
+    equal: (rowVal, ruleVal) => `${rowVal} equals ${ruleVal}`,
+    not_equal: (rowVal, ruleVal) => `${rowVal} not equals ${ruleVal}`,
+    less: (rowVal, ruleVal) => `${rowVal} < ${ruleVal}`,
+    less_or_equal: (rowVal, ruleVal) => `${rowVal} <=  ${ruleVal}`,
+    greater: (rowVal, ruleVal) => `${rowVal} > ${ruleVal}`,
+    greater_or_equal: (rowVal, ruleVal) => `${rowVal} >= ${ruleVal}`,
+    between: (rowVal, [ruleMinVal, ruleMaxVal]) => `${rowVal} between ${ruleMinVal} and ${ruleMaxVal}`,
+    not_between: (rowVal, [ruleMinVal, ruleMaxVal]) => `${rowVal} not between ${ruleMinVal} and ${ruleMaxVal}`,
+};
+
+const ruleEvaluator = (rule, row) => {
+    if (rule.condition === 'AND') return rule.rules.every(rule => ruleEvaluator(rule, row));
+    if (rule.condition === 'OR') return rule.rules.some(rule => ruleEvaluator(rule, row));
+    return opFns[rule.operator](getRowValue(row, rule.id), rule.value);
+};
+
+const ruleStringEvaluator = rule => {
+    if (rule.condition) return '(' + rule.rules.map(rule => ruleStringEvaluator(rule)).join(` ${rule.condition} `) + ')';
+    if (rule.id === 'result') return `Team ${rule.value === '>' ? 'A' : 'B'} wins`;
+    return opStringFns[rule.operator](labelMap[rule.id], rule.value);
+};
 
 class MatchesTab extends BaseTab {
     constructor(App, tabId) {
@@ -62,46 +125,153 @@ class MatchesTab extends BaseTab {
         $('#filter').click(() => {
             this.updateMatchesTable();
         });
+        
+        $('#filter-advanced').click(() => {
+            if ($('#filter-advanced').hasClass("active")) {
+                $('#filter-advanced').removeClass("active");
+                $('.filter-basic').show();
+                $('.filter-advanced').hide();
+            }
+            else {
+                $('#filter-advanced').addClass("active");
+                $('.filter-basic').hide();
+                $('.filter-advanced').show();
+            }
+        });
 
         $('#filter-clear').click(() => {
-            $('#filter-maps').val('');
-            for (let i = 0; i < 8; i++) {
-                $(`#filter-p${i}`).val('');
+            if ($('#filter-advanced').hasClass("active")) {
+                $('#builder').queryBuilder('reset');
+                $('#builder').one('validationError.queryBuilder', function(e, rule, error, value) {
+                    e.preventDefault();
+                });
+            }
+            else {
+                $('#filter-maps').val('');
+                for (let i = 0; i < 8; i++) {
+                    $(`#filter-p${i}`).val('');
+                }
             }
             this.updateMatchesTable();
+        });
+        
+        const players = await this.App.getPlayers();
+        const playerNames = players.map(player => player.name);
+        const mapNames = Array.from(matches.data.reduce((acc, row) => {
+            acc.add(row[1]);
+            return acc;
+        }, new Set()));
+
+        $('#builder').queryBuilder({
+            filters: [
+                {
+                    id: 'teamA',
+                    label: 'Team A',
+                    type: 'string',
+                    input: 'select',
+                    values: playerNames,
+                    operators: ['contains', 'not_contains'],
+                },
+                {
+                    id: 'teamB',
+                    label: 'Team B',
+                    type: 'string',
+                    input: 'select',
+                    values: playerNames,
+                    operators: ['contains','not_contains'],
+                },
+                {
+                    id: 'result',
+                    label: 'Result',
+                    type: 'string',
+                    input: 'select',
+                    values: ['>','<'],
+                    operators: ['equal','not_equal'],
+                },
+                {
+                    id: 'teamATotal',
+                    label: 'Points A',
+                    type: 'integer',
+                    operators: ['equal','not_equal','less','less_or_equal','greater','greater_or_equal','between','not_between'],
+                },
+                {
+                    id: 'teamBTotal',
+                    label: 'Points B',
+                    type: 'integer',
+                    operators: ['equal','not_equal','less','less_or_equal','greater','greater_or_equal','between','not_between'],
+                },
+                {
+                    id: 'pointDiff',
+                    label: 'Pt. Diff.',
+                    type: 'integer',
+                    operators: ['equal','not_equal','less','less_or_equal','greater','greater_or_equal','between','not_between'],
+                },
+                {
+                    id: 'matchId',
+                    label: 'Match ID',
+                    type: 'integer',
+                    operators: ['equal','not_equal','less','less_or_equal','greater','greater_or_equal','between','not_between'],
+                },
+                {
+                    id: 'map',
+                    label: 'Map',
+                    type: 'string',
+                    input: 'select',
+                    values: mapNames,
+                    operators: ['equal','not_equal'],
+                },
+            ]
         });
     }
 
     async updateMatchesTable() {
+        const isAdvancedFilter = $('#filter-advanced').hasClass("active");
         const matches = await this.App.getMatches();
-        const teams = [[], []];
-        const results = [0, 0];
-        for (let i = 0; i < 8; i++) {
-            const player = $(`#filter-p${i}`).val();
-            if (player) teams[i < 4 ? 0 : 1].push(player);
-        }
-        const filteredMatches = matches.data.filter((row) => {
-            const map = $('#filter-maps').val();
-            if (map && map !== row[1]) return false;
-            if (teams[0].every(p => row[2].indexOf(p) !== -1) && teams[1].every(p => row[6].indexOf(p) !== -1)) {
-                if (row[4] == '>') results[0]++;
-                if (row[4] == '<') results[1]++;
-                return true;
+        let filteredMatches;
+        if (!isAdvancedFilter) {
+            const teams = [[], []];
+            const results = [0, 0];
+            for (let i = 0; i < 8; i++) {
+                const player = $(`#filter-p${i}`).val();
+                if (player) teams[i < 4 ? 0 : 1].push(player);
             }
-            if (teams[1].every(p => row[2].indexOf(p) !== -1) && teams[0].every(p => row[6].indexOf(p) !== -1)) {
-                if (row[4] == '>') results[1]++;
-                if (row[4] == '<') results[0]++;
-                return true;
+            filteredMatches = matches.data.filter((row) => {
+                const map = $('#filter-maps').val();
+                if (map && map !== row[1]) return false;
+                if (teams[0].every(p => row[2].indexOf(p) !== -1) && teams[1].every(p => row[6].indexOf(p) !== -1)) {
+                    if (row[4] == '>') results[0]++;
+                    if (row[4] == '<') results[1]++;
+                    return true;
+                }
+                if (teams[1].every(p => row[2].indexOf(p) !== -1) && teams[0].every(p => row[6].indexOf(p) !== -1)) {
+                    if (row[4] == '>') results[1]++;
+                    if (row[4] == '<') results[0]++;
+                    return true;
+                }
+                return false;
+            });
+            if (teams[0].length || teams[1].length) {
+                $('#filter-result').text(`${teams[0].length ? teams[0].join(',') : 'anyone'} vs ${teams[1].length ? teams[1].join(',') : 'anyone'}: ${results[0]} - ${results[1]}`);
+                $('#filter-result').show();
             }
-            return false;
-        });
-        if (teams[0].length || teams[1].length) {
-            $('#filter-result').text(`${teams[0].length ? teams[0].join(',') : 'anyone'} vs ${teams[1].length ? teams[1].join(',') : 'anyone'}: ${results[0]} - ${results[1]}`);
-            $('#filter-result').show();
+            else {
+                $('#filter-result').hide();
+                $('#filter-result').text('');
+            }
         }
         else {
-            $('#filter-result').hide();
-            $('#filter-result').text('');
+            const rule = $('#builder').queryBuilder('getRules');
+            if (rule) {
+                filteredMatches = matches.data.filter(row => ruleEvaluator(rule, row));
+                const ruleString = ruleStringEvaluator(rule);
+                $('#filter-result').text(ruleString.substring(1, ruleString.length - 1));
+                $('#filter-result').show();
+            }
+            else {
+                filteredMatches = matches.data;
+                $('#filter-result').hide();
+                $('#filter-result').text('');
+            }
         }
         this.table.loadData(filteredMatches);
         this.table.getPlugin('columnSorting').sort({ column: 8, sortOrder: 'desc' });
