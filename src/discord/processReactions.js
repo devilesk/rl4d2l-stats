38738d130f4f965062ae1730dbf9c325a56b0b68
devilesk <devilesk@gmail.com>
2019-10-16 15:31:08 -0400

    handle more than 8 reacts

--- src/discord/processReactions.js
+++ src/discord/processReactions.js
@@ -5,17 +5,20 @@ const getGeneratedTeams = require('./teamgen');
 const connection = require('./connection');
 const { msgHasL4DMention, msgRemainingTimeLeft } = require('./util');
 
+let savedUsers = new Collection();
+
 // when a message less than an hour old that pings L4D role gets 8 reactions, then bot will ping all reactors.
 const processReactions = (client, messageCache) => async (msg) => {
     if (msg.channel.name !== config.settings.inhouseChannel) return;
     if (msgHasL4DMention(msg)) {
-        const users = await messageCache.fetchMessageReactionUsers(msg); // fetch users because msg.reactions not updated when admin removes a react
+        let users = await messageCache.fetchMessageReactionUsers(msg); // fetch users because msg.reactions not updated when admin removes a react
         // const users = msg.reactions.reduce((acc, reaction) => acc.concat(reaction.users), new Collection());
         if (!users.has(client.user.id)) { // check if bot has not reacted to message
             if (msgRemainingTimeLeft(msg) > 0) {
                 if (messageCache.isLatest(msg)) {
                     logger.info(`processing message ${msg.id} with ${users.size} reacts...`);
                     if (users.size < 8) {
+                        savedUsers = users.clone();
                         await msg.channel.setTopic(
                             `${users.size} ${users.size === 1 ? 'react' : 'reacts'}. React here to play: https://discordapp.com/channels/${msg.guild.id}/${msg.channel.id}/${msg.id}.` +
                             (users.size > 0 ? `\nReactors: ${users.array().map(user => msg.guild.member(user).displayName).join(', ')}` : '')
@@ -24,11 +27,17 @@ const processReactions = (client, messageCache) => async (msg) => {
                     }
                     else {
                         await msg.react('✅'); // bot reacts to message to prevent pinging reactors again if reactions change later
-                        if (users.size === 8) {
-                            logger.info('8 reactions detected...');
-                            await msg.channel.send(users.array().join(' '), await getGeneratedTeams(process.env.DATA_DIR, connection, users.map(user => user.id), null, true, true));
-                            await msg.channel.setTopic(config.strings.server);
+                        logger.debug(`${users.size} reactions detected... ${users.array().map(user => user.id).join(',')}`);
+                        // if more than 8 users, then randomly select among last users.
+                        if (users.size > 8) {
+                            logger.debug(`${savedUsers.size} savedUsers: ${savedUsers.array().map(user => user.id).join(',')}`);
+                            const lastUsers = users.filter(user => !savedUsers.has(user.id));
+                            const randomUsers = lastUsers.random(8 - savedUsers.size);
+                            logger.debug(`randomly selected ${randomUsers.array().map(user => user.id).join(',')} from ${lastUsers.array().map(user => user.id).join(',')}`);
+                            users = savedUsers.concat(randomUsers);
                         }
+                        await msg.channel.send(users.array().join(' '), await getGeneratedTeams(process.env.DATA_DIR, connection, users.map(user => user.id), null, true, true));
+                        await msg.channel.setTopic(config.strings.server);
                         messageCache.uncacheMessage(msg);
                     }
                 }
